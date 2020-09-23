286367c6c20e5f9409225bae5bd910af71328ad2
MicahXIE <bin.xie.sg@gmail.com>
2020-09-23 20:16:49 +0800

    Leetcode Practice

---
+++ leetcode/Java/Array/LT350_Intersection_of_Two_Arrays_II.java
@@ -0,0 +1,27 @@
+class Solution {  // time O(m + n)
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums1){
+            int freq = map.getOrDefault(i, 0);
+            map.put(i, freq + 1);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i : nums2){
+            if(map.get(i) != null && map.get(i) > 0){
+                list.add(i);
+                map.put(i, map.get(i) - 1);
+            }
+        }
+    
+        // 2ms 98.59%
+        int[] ret = new int[list.size()]; 
+        for(int i = 0; i < list.size();i++){
+            ret[i] = list.get(i);
+        }
+        
+        // 4ms 38.40%
+        //int[] ret = list.stream().mapToInt(k -> k).toArray();
+        
+        return ret;
+    }
+}
